62a7521b2021aa4292645e0cf353283ee958b116
BrenoMelo-Profissional <caio2001ti@gmail.com>
2024-10-29 20:47:54 -0300

    TO DO

--- main/models.py
+++ main/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+class Registro(models.Model):
+    nome = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=15)
+    
+
+
+
+
+
 class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.title
+    
+
+    
--- main/views.py
+++ main/views.py
@@ -1,14 +1,36 @@
-from django.shortcuts import render,redirect
-from .models import Task
+from django.shortcuts import render, redirect
+from .models import Task , Registro
 
+# exibir informaçoes para o front
 def task_list(request):
     tasks = Task.objects.all()
-    return render(request,'tasks/tasks_list.html', {'tasks':tasks})
+    registro = Registro.objects.all()
+    return render(request,'tasks/tasks_list.html', {'tasks': tasks , 'registro': registro})
+
+def cadastro(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        telefone = request.POST.get('telefone')
+        
+        Registro.objects.create(nome=nome, telefone=telefone)
+        return redirect('add_task')
+    return render(request,'tasks/cadastro_task.html')
+
 
 def add_task(request):
     if request.method == 'POST':
         title = request.POST.get('title')
+        completed = request.POST.get('completed')
+        if completed :
+            completed = True
+        else :
+            completed = False
+
+        
         if title:
-            Task.objects.create(title=title)
-            return redirect('task_list')
-        return render(request,'tasks/add_task.html')
+            Task.objects.create(title=title, completed=completed)
+        return redirect('task_list')
+    return render(request,'tasks/add_task.html')
+
+
+
